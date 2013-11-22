13b2adf40e2411b60e42687e038c40ef1875c1f1
Simone Pescina <s.pescina@gmail.com>
2013-11-22 17:33:42 +0100

    Update jquery.flexslider.js
    test fix slide on ios7

--- jquery.flexslider.js
+++ jquery.flexslider.js
@@ -425,25 +425,20 @@
 
             function onTouchMove(e) {
               // Local vars for X and Y points.
-
-              localX = e.touches[0].pageX;
-              localY = e.touches[0].pageY;
-
-              dx = (vertical) ? startX - localY : startX - localX;
-              scrolling = (vertical) ? (Math.abs(dx) < Math.abs(localX - startY)) : (Math.abs(dx) < Math.abs(localY - startY));
-
-              var fxms = 500;
-
-              if ( ! scrolling || Number( new Date() ) - startT > fxms ) {
-                e.preventDefault();
-                if (!fade && slider.transitions) {
-                  if (!slider.vars.animationLoop) {
-                    dx = dx/((slider.currentSlide === 0 && dx < 0 || slider.currentSlide === slider.last && dx > 0) ? (Math.abs(dx)/cwidth+2) : 1);
-                  }
-                  slider.setProps(offset + dx, "setTouch");
-                }
-              }
-            }
+    				  dx = (vertical) ? startX - e.touches[0].pageY : startX - e.touches[0].pageX;
+    				  scrolling = (vertical) ? (Math.abs(dx) < Math.abs(e.touches[0].pageX - startY)) : (Math.abs(dx) < Math.abs(e.touches[0].pageY - startY));
+    				  if (!scrolling || Number(new Date()) - startT > 500) {
+    					  e.preventDefault();
+    					  if (!fade && slider.transitions) {
+    						  if (!vars.animationLoop) {
+    							  dx = dx/((slider.currentSlide === 0 && dx < 0 || slider.currentSlide === slider.last && dx > 0) ? (Math.abs(dx)/cwidth+2) : 1);
+    						  }
+    						  slider.setProps(offset + dx, "setTouch");
+  					    }
+    				  } else {
+    					  el.removeEventListener('touchmove', onTouchMove, false);
+    				  }
+    	      }
 
             function onTouchEnd(e) {
               // finish the touch by undoing the touch session
